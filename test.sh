dfb1a832de92830ac23b91c6bcfa55e72377f4d3
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2023-10-02 11:58:34 +0000

    .

--- test.sh
+++ test.sh
@@ -3,16 +3,15 @@
 sudo pacman -Syu
 
 # X11 related
-sudo pacman -S git base-devel xorg-server xorg-xinit libx11 libxft
+sudo pacman -S --no-confirm git base-devel xorg-server xorg-xinit libx11 libxft
 
 # Other
-sudo pacman -S alacritty
+sudo pacman -S --no-confrim alacritty ranger neovim
 
 echo 'finished added packages'
 
 
 # Bytt ut med egen
-git clone https://github.com/Stianlyng/archinstall.git
 cd archinstall/dwm
 make clean install
 
