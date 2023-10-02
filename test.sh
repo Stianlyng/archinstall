d352574da7fef4736ffccc078c62591c5cff65db
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2023-10-02 13:06:56 +0000

    upd

--- test.sh
+++ test.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-sudo pacman -Syu 
+ask_yes_no() {
+    local prompt="$1"
+    local response
 
-# Install essentials
+    read -p "$prompt [Y/n] " response
+
+    # If Enter is pressed, set response to 'Y'
+    : ${response:='Y'}
+
+    case "$response" in
+        [yY][eE][sS]|[yY]|'')
+            return 0 ;;
+        [nN][oO]|[nN])
+            return 1 ;;
+        *)
+            echo "Invalid response."
+            return 2 ;;
+    esac
+}
+
+# Update and install essentials
+sudo pacman -Syu 
 sudo pacman -S --noconfirm base-devel git ranger neovim terminus-font
 
 # Clone dotfiles and configs
 git clone https://github.com/Stianlyng/archinstall.git
 
-read -p "Do you want to use dwm window manager? [Y/n] " response
+if ask_yes_no "Do you want to use dwm window manager?"; then
+    echo "You chose yes."
 
-case "$response" in
-    [yY][eE][sS]|[yY])
-        echo "You chose yes."
-        
-        # Install dwm dependencies and other software
-        sudo pacman -S --noconfirm xorg-server xorg-xinit libx11 libxft libxinerama alacritty
+    # Install dwm dependencies and other software
+    sudo pacman -S --noconfirm xorg-server xorg-xinit libx11 libxft libxinerama alacritty
 
-        # Install dwm
-        cd archinstall/dwm
-        sudo make clean install
-        echo 'Successfully installed dwm'
-        cd ~/
-        cp archinstall/xinitrc ~/.xinitrc
-        ;;
-    [nN][oO]|[nN])
-        echo "You chose no."
-        ;;
-    *)
-        echo "Invalid response."
-        ;;
-esac
+    # Install dwm
+    cd archinstall/dwm
+    sudo make clean install
+    echo 'Successfully installed dwm'
+    cd ~/
+    cp archinstall/xinitrc_dwm ~/.xinitrc
+fi
+
+
