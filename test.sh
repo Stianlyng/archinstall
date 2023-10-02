a6473bb1a166d03688fa2f35cf50829209847459
Stian Lyng <stianlyng@protonmail.com>
2023-10-02 03:23:32 +0200

    added dwm

--- test.sh
+++ test.sh
@@ -8,11 +8,15 @@ sudo pacman -S git base-devel xorg-server xorg-xinit libx11 libxft
 # Other
 sudo pacman -S alacritty
 
+echo 'finished added packages'
+
+
 # Bytt ut med egen
 git clone git@github.com:Stianlyng/archinstall.git
 cd archinstall/dwm
 make clean install
 
+echo 'successfully installed dwm'
 cd ~/
 
 # Copying
