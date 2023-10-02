9315a2025e5f38864143b5175f501169d646f629
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2023-10-02 03:04:25 +0200

    .

--- test.sh
+++ test.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Hello from the script!"
+sudo pacman -Syu
+
+sudo pacman -S git base-devel xorg-server xorg-xinit libx11 libxft
+
+# Bytt ut med egen
+git clone git://git.suckless.org/dwm
+cd dwm
+make clean install
+
+cd 
+
+# Starting dwm automatically
+
+cat > ~/.xinitrc <<'EOF'
+while true
+do
+    xsetroot -name "$(date) $(uptime | sed 's/.*,//')"
+    sleep 1
+done &
+exec dwm
+EOF
