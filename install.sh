ce3d5866dc11813aa71b2f462ff1f9f3078c24ca
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2023-10-11 22:13:17 +0000

    .

--- install.sh
+++ install.sh
@@ -1,63 +1,21 @@
 #!/bin/bash
 
-ask_yes_no() {
-    local prompt="$1"
-    local response
-
-    read -p "$prompt [Y/n] " response
-
-    # If Enter is pressed, set response to 'Y'
-    : ${response:='Y'}
-
-    case "$response" in
-        [yY][eE][sS]|[yY]|'')
-            return 0 ;;
-        [nN][oO]|[nN])
-            return 1 ;;
-        *)
-            echo "Invalid response."
-            return 2 ;;
-    esac
-}
+# add dotfiles 
+cp -r config/* ~/.config # Copy config folder 
+cp .xinitrc ~/.xinitrc
 
 # Update and install essentials
 sudo pacman -Syu 
-sudo pacman -S --noconfirm base-devel git ranger neovim terminus-font zsh ly
+sudo pacman -S --noconfirm base-devel git ranger neovim terminus-font zsh ly fish alacritty firefox pfmanfm xorg-server xorg-xinit libx11 libxft libxinerama dmenu
 
 # Enable ly display manager
 sudo systemctl enable ly
 
 # Change default shell
-if ask_yes_no "Do you want to install fish shell?"; then
-    sudo pacman -S --noconfirm fish
-    chsh -s /bin/fish
-fi
+chsh -s /bin/fish
 
+# Install dwm
+cd dwm
+sudo make clean install
 
-# Clone dotfiles and configs
-git clone https://github.com/Stianlyng/archinstall.git
-
-# add dotfiles 
-cp -r archinstall/config_cli ~/.config # default dotfiles
-
-if ask_yes_no "Do you want to use a window manager?";then
-    sudo pacman -S --noconfirm alacritty firefox pfmanfm 
-fi
-
-if ask_yes_no "Do you want to use dwm window manager?"; then
-    echo "You chose yes."
-
-    # Install dwm dependencies and other software
-    sudo pacman -S --noconfirm xorg-server xorg-xinit libx11 libxft libxinerama dmenu
-
-    # Install dwm
-    cd archinstall/dwm
-    sudo make clean install
-    echo 'Successfully installed dwm'
-    cd ~/
-    cp archinstall/xinitrc_dwm ~/.xinitrc
-fi
-
-if ask_yes_no "The installer is finished, do you want to log out for the canges to make effect?"; then
-    exit
-fi
+echo 'End of script!'
