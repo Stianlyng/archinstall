f0fc97f60d6db4a084fb5dc98ed0aea3f54e77d5
Stian Lyng <stianlyng@protonmail.com>
2023-10-18 22:07:28 +0200

    .

--- main.sh
+++ main.sh
@@ -1,49 +1,34 @@
 #!/bin/bash
 
 # Install yay
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 # Update and install essentials
-pacman -Syu --noconfirm base-devel git ranger neovim ly fish alacritty pcmanfm tldr bspwm sxhkd 
+sudo pacman -Syu --noconfirm base-devel git ranger neovim ly fish alacritty pcmanfm xclip tldr bspwm sxhkd firefox
 
 # Enable ly display manager
-systemctl enable ly
+sudo systemctl enable ly
 
 # Change default shell
-chsh -s /bin/fish
+sudo chsh -s /bin/fish
 
-# CONFIG FILES
+# Install dwm
+sudo mkdir -p /usr/share/xsessions
 
-
-# BSPWM
-
-mkdir -p ~/.config/bspwm
-cat <<EOL >> ~/.config/bspwm/bspwmrc
-#! /bin/sh
-
-pgrep -x sxhkd > /dev/null || sxhkd &
-
-bspc monitor DP-0 -d 1 2 3 4 5 
-bspc monitor HDMI-0 -d 6 7 8 9
-
-bspc config border_width         4
-bspc config window_gap          12
-bspc config top_padding 	30
-
-bspc config split_ratio          0.52
-bspc config borderless_monocle   true
-bspc config gapless_monocle      true
-bspc config pointer_follows_focus true
-bspc config focus_follows_pointer true
-
-# BORDER
-
-bspc config focused_border_color	"#8be9fd"
-bspc config normal_border_color		"#282a36"
-bspc config active_border_color		"#ff79c6"
+cat <<EOL >> /usr/share/xsessions/dwm.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=dwm
+Comment=Dynamic window manager
+Exec=dwm
+Icon=dwm
+Type=XSession
 EOL
+
+cd dwm
+sudo make clean install
 
 echo 'End of script!'
